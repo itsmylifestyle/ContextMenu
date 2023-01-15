d2e93b697869c5a956dde25a921702b276c47945
itsmylifestyle <hahaii00@mail.ru>
2023-01-15 21:41:04 +0600

    Initial Commit

---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Айбек on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
